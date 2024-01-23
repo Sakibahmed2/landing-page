4e1f10d82952f46f45a3c5c2742e58016a218dfb
unknown <mauijaloskor@gmail.com>
2024-01-23 21:20:54 +0600

    font-family: 'Poppins', sans-serif;

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,9 +2,7 @@ import React, { ReactNode } from "react";
 
 const Container = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-full max-w-[1220px] h-full px-[20px]  mx-auto bg-green-500">
-      {children}
-    </div>
+    <div className="w-full max-w-[1220px]  px-[20px]  mx-auto ">{children}</div>
   );
 };
 
--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -5,14 +5,12 @@ import { Button } from "../ui/button";
 const Navbar = () => {
   return (
     <header className=" h-16 ">
-      <nav className="w-full max-w-[1220px] h-full px-[20px]  mx-auto bg-green-500 flex justify-between items-center">
+      <nav className="w-full max-w-[1220px] h-full px-[20px]  mx-auto  flex justify-between items-center">
         <span className="text-3xl">iRepair</span>
         <ul className="space-x-5">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
-          <Button>
-            <NavLink to="/">Login</NavLink>
-          </Button>
+          <Button>Login</Button>
         </ul>
       </nav>
     </header>
