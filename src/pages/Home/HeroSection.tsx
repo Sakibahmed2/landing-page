13c54acab94408003d6bee8888a0fd90525168d5
unknown <mauijaloskor@gmail.com>
2024-01-24 12:04:17 +0600

    Responsive Hero Section

--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -5,9 +5,9 @@ import macbook from "@/assets/images/macbook-exposed.png";
 
 const HeroSection = () => {
   return (
-    <Container className="h-[calc(100vh-64px)] grid grid-cols-2 place-content-center ">
+    <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center ">
       <div>
-        <h1 className="text-8xl font-extrabold ">
+        <h1 className="text-5xl lg:text-8xl font-bold ">
           <span className="text-gray">Don't worry,</span>
           <br />
           <span className="text-[#1D1D1F]">We'll fix it</span>
@@ -24,8 +24,12 @@ const HeroSection = () => {
         </p>
         <Button>Book a service</Button>
       </div>
-      <div>
-        <img src={macbook} alt="" />
+      <div className="mt-10 w-3/4 lg:w-full mx-auto">
+        <img
+          className="-rotate-[30deg] h-[95%] object-contain"
+          src={macbook}
+          alt=""
+        />
       </div>
     </Container>
   );
