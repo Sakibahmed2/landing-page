45de5fabdaf65c64ea1f191659c415f3c439f5fc
unknown <mauijaloskor@gmail.com>
2024-01-25 20:40:23 +0600

    Setup Admin Layout

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,9 @@
 import App from "@/App";
+import AdminLayout from "@/components/layouts/AdminLayout";
 import About from "@/pages/About";
+import AddService from "@/pages/Admin/AddService";
+import Dashboard from "@/pages/Admin/Dashboard";
+import ServiceList from "@/pages/Admin/ServiceList";
 import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router-dom";
 
@@ -15,6 +19,24 @@ const routes = createBrowserRouter([
       {
         path: "about",
         element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: "service-list",
+        element: <ServiceList />,
+      },
+      {
+        path: "add-service",
+        element: <AddService />,
       },
     ],
   },
