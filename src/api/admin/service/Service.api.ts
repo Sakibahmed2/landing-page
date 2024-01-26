8ab91f77b0aedc7d4db0a20bb6bfe0f58bbc7b4c
unknown <mauijaloskor@gmail.com>
2024-01-26 12:02:03 +0600

    Error Handling With TanStack Query

--- src/api/admin/service/Service.api.ts
+++ src/api/admin/service/Service.api.ts
@@ -1,5 +1,17 @@
+import axios from "axios";
+
 export const getServices = async () => {
-  return await fetch(
-    "https://irepair-backend-rho.vercel.app/api/v1/services"
-  ).then((res) => res.json());
+  return axios.get("https://irepair-backend-rho.vercel.app/api/v1/serdfdvices");
 };
+
+// export const getServices = async () => {
+//   const res = await fetch(
+//     "https://irepair-backend-rho.vercel.app/api/v1/serdfdvices"
+//   ).then((res) => res.json());
+
+//   if (!res.success) {
+//     throw new Error("Something went wrong");
+//   }
+
+//   return res;
+// };
--- src/pages/Admin/ServiceList.tsx
+++ src/pages/Admin/ServiceList.tsx
@@ -3,21 +3,24 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const ServiceList = () => {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["services"],
     queryFn: getServices,
   });
 
+  console.log({ isLoading, data, isError });
   if (isLoading) {
     return <p>Loading...........</p>;
   }
 
-  console.log({ isLoading, data });
+  if (isError) {
+    return <p>Something went wrong</p>;
+  }
 
   return (
     <div>
-      {data?.data.map((item) => (
-        <h1>{item.name}</h1>
+      {data?.data?.map((item) => (
+        <h1>{item?.name}</h1>
       ))}
     </div>
   );
