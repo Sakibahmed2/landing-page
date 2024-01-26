1a94915a151bc20427a78c67b27ebf4da7c94821
unknown <mauijaloskor@gmail.com>
2024-01-26 16:15:53 +0600

    Post Data Using TanStack Query

--- src/pages/Admin/AddService.tsx
+++ src/pages/Admin/AddService.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
+import React, { FormEvent, useState } from "react";
 
 const AddService = () => {
+  const [serviceName, setServiceName] = useState("");
+
+  const { mutateAsync, isError, isSuccess } = useMutation({
+    mutationFn: async (data) => {
+      return await fetch(
+        "https://irepair-backend-rho.vercel.app/api/v1/services",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+    },
+  });
+
+  console.log({ isError, isSuccess });
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const serviceData = {
+      name: serviceName,
+      description: "Description",
+      devices: ["iPhone", "android"],
+      price: 1000,
+    };
+
+    console.log(serviceData);
+    await mutateAsync(serviceData);
+    console.log("done");
+  };
+
   return (
     <div>
-      <h1>Add service list</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setServiceName(e.target.value)} />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 };
